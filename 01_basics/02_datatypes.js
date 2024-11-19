12879d7eb955488efe5a6098804d7053eaadb68f
shrikant <shrikantsaraswat50@gmail.com>
2024-11-19 09:30:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all code as newer js
+
+// alert( 3 + 3)  // we are using node js not browser
+console.log(3 +
+    3); // code readability should be high
+
+console.log("Shrikant")
+
+let name = "shrikant"; // String
+let age = 18 // number
+let isLoggedIn = false // boolean
+
+// string
+// number
+// boolean => true/false
+// bigint
+// null => standalone value and Object
+// undefined 
+// symbol => unique
+
+//object
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
